a1c603dbd9c63c1540a82e1bd3125850f024f8a0
CtriXin <draftdog@foxmail.com>
2016-12-23 10:43:36 +0800

    因为pc和mac不知道哪个文件不兼容 所以备份 后续的提交均为这个

--- project/project/48hour/assets/js/page-loaded.js
+++
@@ -1,9 +0,0 @@
-//$(document).ready(function(){
-//    function button(){
-//        $('#setAllowScrollingTrue').click()
-//    };
-//    setInterval(button,19000);
-//});
-$('.btn-question').click(function(){
-    $(this).attr('data-value',$(this).text()).css({'background': '#eb472d','border-color':'#eb472d','color':'#fff'}).siblings().removeAttr('data-value').css({'background': 'none','border-color':'#fff','color':'#fff'})
-});
